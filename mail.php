57d1fb7f2e568b579213d5c5de6e912861b86bda
smallik1995 <smallik666@outlook.com>
2019-08-28 17:06:02 +0300

    Add files via upload

--- mail.php
+++ mail.php
@@ -18,7 +18,7 @@ $msg  = "<html><body style='font-family: Arial, sans-serif; font-size: 2vw;'>";
 $msg .= "<h2 style='font-weight: bold;'>Данные клиента:</h2>\r\n";
 $msg .= "<p><strong>Имя:</strong> ".$userName."</p>\r\n";
 $msg .= "<p><strong>E-mail:</strong> ".$userMail."</p>\r\n";
-$msg .= "<p>".$userText."</p>\r\n"
+$msg .= "<p>".$userText."</p>\r\n";
 $msg .= "</body></html>";
 
 // отправка сообщения
