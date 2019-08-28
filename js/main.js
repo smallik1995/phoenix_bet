8908ae39781670925d67c13809ce6abaedf806c1
smallik1995 <smallik666@outlook.com>
2019-08-28 16:50:34 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-VK.Widgets.CommunityMessages("vk_community_messages", 113323805, {expanded: "1",tooltipButtonText: "Есть вопрос?"});
+VK.Widgets.CommunityMessages("vk_community_messages", 171148664, {expanded: "1",tooltipButtonText: "Есть вопрос?"});
+
+$(document).ready(function() {
+	$(".form").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$(".form").trigger("reset");
+		});
+		return false;
+	});
+});
